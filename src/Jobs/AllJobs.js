ca746e855371d694bcac565d49e23fc825e5db24
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2021-05-03 08:31:28 +0100

    updated project

---
+++ src/Jobs/AllJobs.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class AllJobs extends React.Component {
+  state = {
+    jobs: []
+  }
+
+  componentDidMount() {
+    axios.get(`https://remoteok.io/api`)
+      .then(res => {
+        const jobs = res.data;
+        this.setState({ jobs });
+      })
+  }
+
+  render() {
+    return (
+    <div className="job">
+        { this.state.jobs.map(job => 
+        <ul>
+            <li>{job.id}</li>
+            <li>{job.company}</li>
+            <li>{job.position}</li> 
+            <a href={job.apply_url} target="_blank" rel="noreferrer" >Apply Here</a>
+        </ul>
+        )}
+    </div>
+    )
+  }
+}
--- src/Jobs/Jobs.js
+++ src/Jobs/Jobs.js
@@ -1,30 +1,32 @@
 import React from 'react'
-import axios from 'axios';
+import AllJobs from './AllJobs'
+// import axios from 'axios';
 
 
 function Jobs() {
 
+// axios.get('https://remoteok.io/api')
+//   .then(function (response) {
+//     // handle success
+//     console.log(response);
+//   })
+//   .catch(function (error) {
+//     // handle error
+//     console.log(error);
+//   })
+//   .then(function () {
+//     // always executed
+//   });
 
 
-
-axios.get('https://remoteok.io/api')
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .then(function () {
-    // always executed
-  });
 
 
 
     return (
         <div>
-            <h1>The Jobs</h1>
+
+            <h1>The Jobss</h1>
+            <AllJobs />
         </div>
     )
 }
